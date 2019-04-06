f22eac08d2d1e7ed883aa07f0c6a5470ee908cda
rishi1@codesamurai.club <rishi1@codesamurai.club>
2019-04-06 13:55:00 -0400

    hackathon 2019

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ function databaseInitialize() {
         User = db.addCollection("users");
         User.insert({username:'admin',password:'admin'});
         User.insert({username:'user',password:'user'});
+
     }
+     User.insert({username:'rishi',password:'rishi123'});
     if (Item === null) {
         Item = db.addCollection('items');
     }
@@ -109,9 +111,17 @@ app.post('/login', function (request, response) {
     request.session.user = loginName;
 
     //hint: check is password is good or not, if not load same page with error as below
+    var login = userPasswordMatch(loginName,password);
+    if(login){
+
+        response.render('listpage', {items: Item.find()});
+    }
+    else{
+        response.render('index', {message: "Invalid user name or password"});
+
+    }
     //response.render('index', {message: "Invalid user name or password"});
 
-    response.render('listpage', {items: Item.find()});
 
 });
 
@@ -121,8 +131,10 @@ app.post('/login', function (request, response) {
 app.post('/saveitem', function (request, response) {
 
     // hint #1: find the helper function that will help save the information first
+    var items= saveFormAndReturnAllItems(request.body)
+
     // hint #2: make sure to send the list of items to the list page
 
-    response.render('listpage',{ items:[] });
+    response.render('listpage',{ items:items });
 });
 
